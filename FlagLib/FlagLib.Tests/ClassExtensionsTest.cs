85f3d7c68f83b6240feae9518d742ccbfd31d4d7
Dennis Daume <daume.dennis@gmail.com>
2011-08-09 01:15:43 +0200

    Added new extension method "ThrowIfIsNull" and its test methods

---
+++ FlagLib/FlagLib.Tests/ClassExtensionsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FlagLib.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlagLib.Tests
+{
+    /// <summary>
+    ///This is a test class for ClassExtensionsTest and is intended
+    ///to contain all ClassExtensionsTest Unit Tests
+    ///</summary>
+    [TestClass]
+    public class ClassExtensionsTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThrowIfIsNullTest()
+        {
+            object testObject = null;
+
+            testObject.ThrowIfIsNull("testClass");
+        }
+
+        [TestMethod]
+        public void ThrowIfIsNullInverseTest()
+        {
+            object testObject = new object();
+
+            testObject.ThrowIfIsNull("testClass");
+        }
+    }
+}
---
+++ FlagLib/FlagLib/Extensions/ClassExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FlagLib.Extensions
+{
+    public static class ClassExtensions
+    {
+        /// <summary>
+        /// Throws an <see cref="System.ArgumentNullException"/> if the object is null.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="object">The object.</param>
+        /// <param name="parameterName">Name of the parameter.</param>
+        public static void ThrowIfIsNull<T>(this T @object, string parameterName) where T : class
+        {
+            if (@object == null)
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+        }
+    }
+}
