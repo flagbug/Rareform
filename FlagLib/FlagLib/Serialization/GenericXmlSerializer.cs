12459b150ca3b82f7be525c33ccaed4dc150c2b5
Dennis Daume <daume.dennis@gmail.com>
2011-08-08 21:53:04 +0200

    Renamed methods (this breaks the API)

--- FlagLib/FlagLib/Serialization/GenericXmlSerializer.cs
+++ FlagLib/FlagLib/Serialization/GenericXmlSerializer.cs
@@ -11,28 +11,28 @@ namespace FlagLib.Serialization
     public static class GenericXmlSerializer
     {
         /// <summary>
-        /// Saves the specified collection at the specified path.
+        /// Serializes the specified collection at the specified path.
         /// </summary>
         /// <typeparam name="T">Type of the items to serialize</typeparam>
-        /// <param name="items">The items tho serialize.</param>
+        /// <param name="collection">The items tho serialize.</param>
         /// <param name="path">The path of the file.</param>
-        public static void SaveCollection<T>(ICollection<T> items, string path) where T : class, new()
+        public static void SerializeCollection<T>(ICollection<T> collection, string path) where T : class, new()
         {
-            XmlSerializer serializer = new XmlSerializer(items.GetType());
+            XmlSerializer serializer = new XmlSerializer(collection.GetType());
 
             using (TextWriter writer = new StreamWriter(path, false))
             {
-                serializer.Serialize(writer, items);
+                serializer.Serialize(writer, collection);
             }
         }
 
         /// <summary>
-        /// Saves the specified item at the specified path.
+        /// Serializes the specified item to the specified path.
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="item">The item to serialize.</param>
         /// <param name="path">The path of the file.</param>
-        public static void SaveSingle<T>(T item, string path) where T : class, new()
+        public static void SerializeItem<T>(T item, string path) where T : class, new()
         {
             XmlSerializer serializer = new XmlSerializer(item.GetType());
 
@@ -43,12 +43,14 @@ namespace FlagLib.Serialization
         }
 
         /// <summary>
-        /// Reads the items at the specified path.
+        /// Deserializes the collection from the specified path.
         /// </summary>
         /// <typeparam name="T">The type of the serialized items</typeparam>
         /// <param name="path">The path of the file.</param>
-        /// <returns>The deserialized items</returns>
-        public static IEnumerable<T> ReadEnumerable<T>(string path) where T : class, new()
+        /// <returns>
+        /// The deserialized collection.
+        /// </returns>
+        public static ICollection<T> Deserializecollection<T>(string path) where T : class, new()
         {
             List<T> items = new List<T>();
 
@@ -74,12 +76,14 @@ namespace FlagLib.Serialization
         }
 
         /// <summary>
-        /// Reads the item at the specified path.
+        /// Deserializes the item at the specified path.
         /// </summary>
         /// <typeparam name="T">The type of the serialized item</typeparam>
         /// <param name="path">The path of the file.</param>
-        /// <returns>The deserialized item.</returns>
-        public static T ReadSingle<T>(string path) where T : class, new()
+        /// <returns>
+        /// The deserialized item.
+        /// </returns>
+        public static T DeserializeItem<T>(string path) where T : class, new()
         {
             T item = new T();
 
