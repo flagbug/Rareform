d74ae62ab12ddf695173f41dc4e3581cbc1bb481
Dennis Daume <daume.dennis@gmail.com>
2011-08-09 00:09:05 +0200

    Added tests for ReflectionUtilities class

---
+++ FlagLib/FlagLib.Tests/ReflectionUtilitiesTest.cs
@@ -0,0 +1,52 @@
+﻿using FlagLib.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlagLib.Tests
+{
+    /// <summary>
+    ///This is a test class for ReflectionUtilitiesTest and is intended
+    ///to contain all ReflectionUtilitiesTest Unit Tests
+    ///</summary>
+    [TestClass]
+    public class ReflectionUtilitiesTest
+    {
+        private int testMember = 0;
+
+        private int TestMember { get; set; }
+
+        [TestMethod]
+        public void GetMemberNameLocalScopeTest()
+        {
+            int testMember = 0;
+
+            string expected = "testMember";
+            string actual;
+
+            actual = ReflectionUtilities.GetMemberName(() => testMember);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetMemberNameFieldTest()
+        {
+            string expected = "testMember";
+            string actual;
+
+            actual = ReflectionUtilities.GetMemberName(() => this.testMember);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetMemberNamePropertyTest()
+        {
+            string expected = "TestMember";
+            string actual;
+
+            actual = ReflectionUtilities.GetMemberName(() => this.TestMember);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
