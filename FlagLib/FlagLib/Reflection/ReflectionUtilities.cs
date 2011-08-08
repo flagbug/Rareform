fed88dea9545dcde7faa7d6ec24061b61ca5ad25
Dennis Daume <daume.dennis@gmail.com>
2011-08-08 23:54:32 +0200

    Added method to retrieve the name of a member

---
+++ FlagLib/FlagLib/Reflection/ReflectionUtilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace FlagLib.Reflection
+{
+    public static class ReflectionUtilities
+    {
+        /// <summary>
+        /// Gets the name of the member.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression">The expression.</param>
+        /// <returns></returns>
+        public static string GetMemberName<T>(Expression<Action<T>> expression)
+        {
+            MemberExpression memberExpression = expression.Body as MemberExpression;
+
+            return memberExpression.Member.Name;
+        }
+    }
+}
