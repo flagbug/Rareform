15e6ba81cc1a65ddd0ab4090a29803f7ea749d2e
Dennis Daume <daume.dennis@gmail.com>
2011-08-09 00:11:04 +0200

    Improved documentation

--- FlagLib/FlagLib/Reflection/ReflectionUtilities.cs
+++ FlagLib/FlagLib/Reflection/ReflectionUtilities.cs
@@ -6,11 +6,11 @@ namespace FlagLib.Reflection
     public static class ReflectionUtilities
     {
         /// <summary>
-        /// Gets the name of the member.
+        /// Gets the name of a member which is passed via the expression.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="expression">The expression.</param>
-        /// <returns></returns>
+        /// <typeparam name="T">Type of the member.</typeparam>
+        /// <param name="expression">The expression which contains the member.</param>
+        /// <returns>The members name.</returns>
         public static string GetMemberName<T>(Expression<Func<T>> expression)
         {
             MemberExpression memberExpression = expression.Body as MemberExpression;
