f2f93157cec8c0862741d7dfb1b40799eed0ea4d
Dennis Daume <daume.dennis@gmail.com>
2011-08-09 15:42:27 +0200

    Added null checks

--- FlagLib/FlagLib/Extensions/IntegerExtensions.cs
+++ FlagLib/FlagLib/Extensions/IntegerExtensions.cs
@@ -12,6 +12,8 @@ namespace FlagLib.Extensions
         /// <param name="parameterName">Name of the parameter.</param>
         public static void ThrowIfIsGreaterThan(this int value, int limit, string parameterName)
         {
+            parameterName.ThrowIfIsNull("parameterName");
+
             if (value > limit)
                 throw new ArgumentOutOfRangeException(parameterName, "Value must be less than" + limit);
         }
@@ -24,6 +26,8 @@ namespace FlagLib.Extensions
         /// <param name="parameterName">Name of the parameter.</param>
         public static void ThrowIfIsLessThan(this int value, int limit, string parameterName)
         {
+            parameterName.ThrowIfIsNull("parameterName");
+
             if (value < limit)
                 throw new ArgumentOutOfRangeException(parameterName, "Value must be greater than" + limit);
         }
