3b8c3d366d98ecaed1bd1f10b2bb63324f8b62df
Dennis Daume <daume.dennis@gmail.com>
2011-08-25 00:36:57 +0200

    Added a console test project

---
+++ FlagLib/FlagLib.ConsoleTests/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using FlagLib.IO;
+
+namespace FlagLib.ConsoleTests
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\Dennis\Sync Test Source\Matrix.mkv";
+            string targetPath = @"C:\Users\Dennis\Sync Test Target\Matrix.mkv";
+
+            using (FileStream sourceStream = new FileInfo(sourcePath).OpenRead())
+            {
+                using (FileStream targetStream = File.Create(targetPath))
+                {
+                    StreamCopyOperation operation = new StreamCopyOperation(sourceStream, targetStream, 512 * 1024, true);
+
+                    operation.CopyProgressChanged += (sender, e) =>
+                        {
+                            System.Console.WriteLine(DateTime.Now.ToLongTimeString() + " " + e.AverageSpeed / 1024 / 1024 + " MB/s " + e.TotalCopiedBytes / 1024 / 1024 + " MB");
+                        };
+
+                    operation.CopyStream();
+                }
+            }
+        }
+    }
+}
