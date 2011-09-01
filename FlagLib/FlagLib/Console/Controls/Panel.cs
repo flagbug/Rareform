13fb8e873fb22da8189e77f416074cd93eb63f6d
Dennis Daume <daume.dennis@gmail.com>
2011-09-01 20:43:16 +0200

    Moved all console-related classes into it's own project: FlagConsole - https://bitbucket.org/Flagbug/flagconsole

--- FlagLib/FlagLib/Console/Controls/Panel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FlagLib.Console.Controls
-{
-    /// <summary>
-    /// Provides a <see cref="Panel"/> that can contain controls
-    /// </summary>
-    public class Panel : Container
-    {
-    }
-}
