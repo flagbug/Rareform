ccfef5694770132cd7aee3a485e913ee5c7ab0b2
Dennis Daume <daume.dennis@gmail.com>
2011-09-26 23:19:15 +0200

    Added an overload method for "ThrowIfNull", which receives instead of a hard-coded string as parameter name, an expression.
    For example:
    
    object test = null;
    test.ThrowIfNull("test");
    
    can now be written as:
    
    object test = null;
    test.ThrowIfNull(() => test);
    
    This has the advantage of no longer need of a hard-coded string.

--- FlagLib/FlagLib/Extensions/ClassExtensions.cs
+++ FlagLib/FlagLib/Extensions/ClassExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq.Expressions;
+using FlagLib.Reflection;
 
 namespace FlagLib.Extensions
 {
@@ -15,6 +17,20 @@ namespace FlagLib.Extensions
             if (@object == null)
             {
                 throw new ArgumentNullException(parameterName);
+            }
+        }
+
+        /// <summary>
+        /// Throws an <see cref="System.ArgumentNullException"/> if the object is null.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="object">The object.</param>
+        /// <param name="parameterName">The expression, which resolves to the parameter name.</param>
+        public static void ThrowIfNull<T>(this T @object, Expression<Func<T>> parameterName) where T : class
+        {
+            if (@object == null)
+            {
+                throw new ArgumentNullException(Reflector.GetMemberName(parameterName));
             }
         }
     }
