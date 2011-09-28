d3a705b8bc3306ff1bc1a81b051f908d354b365d
Dennis Daume <daume.dennis@gmail.com>
2011-09-28 10:29:57 +0200

    Added unit tests for LongExtensions and RelayCommand

---
+++ FlagLib/FlagLib.Tests/LongExtensionsTest.cs
@@ -0,0 +1,74 @@
+﻿using FlagLib.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlagLib.Tests
+{
+    /// <summary>
+    ///This is a test class for LongExtensionsTest and is intended
+    ///to contain all LongExtensionsTest Unit Tests
+    ///</summary>
+    [TestClass]
+    public class LongExtensionsTest
+    {
+        [TestMethod]
+        public void ToSizeStringByteTest()
+        {
+            long size = 512;
+            string expected = "512 B";
+            string actual;
+
+            actual = size.ToSizeString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ToSizeStringKiloByteTest()
+        {
+            long size = 1048;
+            string expected = "2,00 KB";
+            string actual;
+
+            actual = size.ToSizeString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ToSizeStringMegaByteTest()
+        {
+            long size = 1024 * 1024;
+            string expected = "1,00 MB";
+            string actual;
+
+            actual = size.ToSizeString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ToSizeStringGigaByteTest()
+        {
+            long size = 1024 * 1024 * 1024;
+            string expected = "1,00 GB";
+            string actual;
+
+            actual = size.ToSizeString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ToSizeStringTeraByteTest()
+        {
+            long size = 1024L * 1024L * 1024L * 1024L;
+
+            string expected = "1,00 TB";
+            string actual;
+
+            actual = size.ToSizeString();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ FlagLib/FlagLib.Tests/RelayCommandTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using FlagLib.Patterns;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlagLib.Tests
+{
+    /// <summary>
+    ///This is a test class for RelayCommandTest and is intended
+    ///to contain all RelayCommandTest Unit Tests
+    ///</summary>
+    [TestClass]
+    public class RelayCommandTest
+    {
+        [TestMethod]
+        public void CanExecuteTest()
+        {
+            bool expected = true;
+            bool actual;
+
+            Action<object> execute = (arg) =>
+                {
+                };
+
+            Predicate<object> canExecute = (arg) =>
+                {
+                    return true;
+                };
+
+            RelayCommand target = new RelayCommand(execute, canExecute);
+
+            actual = target.CanExecute(null);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ExecuteTest()
+        {
+            bool expected = true;
+            bool actual;
+
+            Action<object> execute = (arg) =>
+            {
+                actual = true;
+            };
+
+            Predicate<object> canExecute = (arg) =>
+            {
+                return true;
+            };
+
+            RelayCommand target = new RelayCommand(execute, canExecute);
+
+            actual = target.CanExecute(null);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
