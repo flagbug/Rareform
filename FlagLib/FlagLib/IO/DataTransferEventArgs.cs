d8e05ec76a6daa82dd912d57b3beb09d83603629
Dennis Daume <daume.dennis@gmail.com>
2011-10-08 13:22:53 +0200

    Added a second DataTransferEventArgs class, which takes two generic parameters that indicate the type of the source and destination

--- FlagLib/FlagLib/IO/DataTransferEventArgs.cs
+++ FlagLib/FlagLib/IO/DataTransferEventArgs.cs
@@ -3,7 +3,7 @@
 namespace FlagLib.IO
 {
     /// <summary>
-    /// Encapsulates informations from a data transfer update.
+    /// Encapsulates informations of a data transfer.
     /// </summary>
     public class DataTransferEventArgs : EventArgs
     {
@@ -40,5 +40,38 @@ namespace FlagLib.IO
             this.TotalBytes = totalBytes;
             this.TransferredBytes = transferredBytes;
         }
+    }
+
+    /// <summary>
+    /// Encapsulates informations of a data transfer.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the source.</typeparam>
+    /// <typeparam name="TDestination">The type of the destination.</typeparam>
+    public class DataTransferEventArgs<TSource, TDestination> : DataTransferEventArgs
+    {
+        /// <summary>
+        /// Gets or sets the source.
+        /// </summary>
+        /// <value>
+        /// The source.
+        /// </value>
+        public TSource Source { get; set; }
+
+        /// <summary>
+        /// Gets or sets the destination.
+        /// </summary>
+        /// <value>
+        /// The destination.
+        /// </value>
+        public TDestination Destination { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataTransferEventArgs"/> class.
+        /// </summary>
+        /// <param name="totalBytes">The total number of bytes.</param>
+        /// <param name="copiedBytes">The number of transferred bytes.</param>
+        public DataTransferEventArgs(long totalBytes, long transferredBytes)
+            : base(totalBytes, transferredBytes)
+        { }
     }
 }
