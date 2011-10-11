1a217125e6c435bee660f43ee281594458de94c9
Dennis Daume <daume.dennis@gmail.com>
2011-10-11 14:00:03 +0200

    Changed the license text a bit

--- FlagLib/FlagLib/Collections/EventCollectionEventArgs.cs
+++ FlagLib/FlagLib/Collections/EventCollectionEventArgs.cs
@@ -1,4 +1,6 @@
 ﻿/*
+ * This source is released under the MIT-license.
+ * 
  * Copyright (c) 2011 Dennis Daume
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
