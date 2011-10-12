b0a5e9be51cfe9f1745093bbec558ead873ae76b
Dennis Daume <daume.dennis@gmail.com>
2011-10-12 11:23:37 +0200

    Release of version 4.1.0

--- FlagLib/FlagLib/Properties/AssemblyInfo.cs
+++ FlagLib/FlagLib/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * This source is released under the MIT-license.
- * 
+ *
  * Copyright (c) 2011 Dennis Daume
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
@@ -54,5 +54,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyVersion("4.1.0.0")]
+[assembly: AssemblyFileVersion("4.1.0.0")]
