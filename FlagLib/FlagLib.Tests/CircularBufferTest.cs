545f1cefab2c435d13a7b2a9abe1d5bc1f1d39c4
Dennis Daume <daume.dennis@gmail.com>
2011-11-24 08:49:40 +0100

    Added a circular buffer

---
+++ FlagLib/FlagLib.Tests/CircularBufferTest.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * This source is released under the MIT-license.
+ *
+ * Copyright (c) 2011 Dennis Daume
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+using System;
+using System.Linq;
+using FlagLib.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlagLib.Tests
+{
+    /// <summary>
+    ///This is a test class for CircularBufferTest and is intended
+    ///to contain all CircularBufferTest Unit Tests
+    ///</summary>
+    [TestClass]
+    public class CircularBufferTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Constructor_ZeroAsCapacity_ThrowsArgumentNullException()
+        {
+            const int capacity = 0;
+            var target = new CircularBuffer<int>(capacity);
+        }
+
+        [TestMethod]
+        public void Add_ThreeAsCapacityAndAddOneItem_CountIsOne()
+        {
+            var target = new CircularBuffer<int>(3) { 1 };
+
+            Assert.IsTrue(target.Count == 1);
+        }
+
+        [TestMethod]
+        public void Add_ThreeAsCapacityAndAddFourItems_CountIsThreeAndFirstItemIsRemoved()
+        {
+            var target = new CircularBuffer<int>(3) { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            Assert.IsTrue(target.Count == 3);
+            Assert.IsTrue(target.Capacity == 3);
+
+            var targetList = target.ToList();
+
+            Assert.IsTrue(targetList[0] == 7);
+            Assert.IsTrue(targetList[1] == 8);
+            Assert.IsTrue(targetList[2] == 6);
+        }
+
+        [TestMethod]
+        public void ClearTest()
+        {
+            var target = new CircularBuffer<int>(3) { 1, 2, 3 };
+
+            target.Clear();
+
+            Assert.IsTrue(target.Capacity == 3);
+            Assert.IsTrue(target.Count == 0);
+        }
+    }
+}
---
+++ FlagLib/FlagLib/Collections/CircularBuffer.cs
@@ -0,0 +1,176 @@
+﻿/*
+ * This source is released under the MIT-license.
+ *
+ * Copyright (c) 2011 Dennis Daume
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using FlagLib.Extensions;
+
+namespace FlagLib.Collections
+{
+    /// <summary>
+    /// Provides a circular buffer.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the <see cref="CircularBuffer{T}"/></typeparam>
+    public class CircularBuffer<T> : ICollection<T>
+    {
+        private readonly List<T> buffer;
+        private int position;
+
+        public int Capacity
+        {
+            get { return this.buffer.Capacity; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CircularBuffer&lt;T&gt;"/> class.
+        /// </summary>
+        /// <param name="capacity">The capacity of the circular buffer.</param>
+        public CircularBuffer(int capacity)
+        {
+            capacity.ThrowIfLessThan(1, () => capacity);
+
+            this.buffer = new List<T>(capacity);
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="T:System.Collections.Generic.IEnumerator`1"/> that can be used to iterate through the collection.
+        /// </returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return buffer.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.IEnumerator"/> object that can be used to iterate through the collection.
+        /// </returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// Adds an item to the <see cref="T:System.Collections.Generic.ICollection`1"/>.
+        /// </summary>
+        /// <param name="item">The object to add to the <see cref="T:System.Collections.Generic.ICollection`1"/>.</param>
+        /// <exception cref="T:System.NotSupportedException">
+        /// The <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only.
+        ///   </exception>
+        public void Add(T item)
+        {
+            if (this.Count < this.Capacity)
+            {
+                this.buffer.Add(item);
+                this.position = this.buffer.Count - 1;
+
+                if (this.position == this.Capacity - 1)
+                {
+                    this.position = 0;
+                }
+            }
+
+            else
+            {
+                this.buffer[this.position] = item;
+                this.position++;
+
+                if (this.position == this.Count)
+                {
+                    this.position = 0;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes all items from the <see cref="T:System.Collections.Generic.ICollection`1"/>.
+        /// </summary>
+        /// <exception cref="T:System.NotSupportedException">
+        /// The <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only.
+        ///   </exception>
+        public void Clear()
+        {
+            this.buffer.Clear();
+        }
+
+        /// <summary>
+        /// Determines whether the <see cref="T:System.Collections.Generic.ICollection`1"/> contains a specific value.
+        /// </summary>
+        /// <param name="item">The object to locate in the <see cref="T:System.Collections.Generic.ICollection`1"/>.</param>
+        /// <returns>
+        /// true if <paramref name="item"/> is found in the <see cref="T:System.Collections.Generic.ICollection`1"/>; otherwise, false.
+        /// </returns>
+        public bool Contains(T item)
+        {
+            return this.buffer.Contains(item);
+        }
+
+        /// <summary>
+        /// Copies the <see cref="CircularBuffer&lt;T&gt;"/> to the specified array, starting at the specified index.
+        /// </summary>
+        /// <param name="array">The destination array.</param>
+        /// <param name="arrayIndex">Index of the <see cref="CircularBuffer&lt;T&gt;"/> where the copy begins.</param>
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            this.buffer.CopyTo(array, arrayIndex);
+        }
+
+        /// <summary>
+        /// Removes the first occurrence of a specific object from the <see cref="T:System.Collections.Generic.ICollection`1"/>.
+        /// </summary>
+        /// <param name="item">The object to remove from the <see cref="T:System.Collections.Generic.ICollection`1"/>.</param>
+        /// <returns>
+        /// true if <paramref name="item"/> was successfully removed from the <see cref="T:System.Collections.Generic.ICollection`1"/>; otherwise, false. This method also returns false if <paramref name="item"/> is not found in the original <see cref="T:System.Collections.Generic.ICollection`1"/>.
+        /// </returns>
+        /// <exception cref="T:System.NotSupportedException">
+        /// The <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only.
+        ///   </exception>
+        public bool Remove(T item)
+        {
+            return this.buffer.Remove(item);
+        }
+
+        /// <summary>
+        /// Gets the number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1"/>.
+        /// </summary>
+        /// <returns>
+        /// The number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1"/>.
+        ///   </returns>
+        public int Count
+        {
+            get { return this.buffer.Count; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only.
+        /// </summary>
+        /// <returns>true if the <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only; otherwise, false.
+        ///   </returns>
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+    }
+}
