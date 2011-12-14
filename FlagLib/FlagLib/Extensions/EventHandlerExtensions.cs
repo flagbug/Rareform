8264977abddb6a57c61556020c756bb572578408
Dennis Daume <daume.dennis@gmail.com>
2011-12-15 00:45:51 +0100

    Marked the "Raise" methods as obsolete and introduces them as renamed to "SafeRaise"

--- FlagLib/FlagLib/Extensions/EventHandlerExtensions.cs
+++ FlagLib/FlagLib/Extensions/EventHandlerExtensions.cs
@@ -29,12 +29,42 @@ namespace FlagLib.Extensions
     public static class EventHandlerExtensions
     {
         /// <summary>
+        /// Checks if the event handler is null and raises it, if not.
+        /// </summary>
+        /// <typeparam name="T">The type of the event argument.</typeparam>
+        /// <param name="handler">The event handler.</param>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="eventArgs">The event args.</param>
+        public static void SafeRaise<T>(this EventHandler<T> handler, object sender, T eventArgs) where T : EventArgs
+        {
+            if (handler != null)
+            {
+                handler(sender, eventArgs);
+            }
+        }
+
+        /// <summary>
+        /// Checks if the event handler is null and raises it, if not.
+        /// </summary>
+        /// <param name="handler">The event handler.</param>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="eventArgs">The event args.</param>
+        public static void SafeRaise(this EventHandler handler, object sender, EventArgs eventArgs)
+        {
+            if (handler != null)
+            {
+                handler(sender, eventArgs);
+            }
+        }
+
+        /// <summary>
         /// Raises the event handler.
         /// </summary>
         /// <typeparam name="T">The type of the event args.</typeparam>
         /// <param name="handler">The event handler.</param>
         /// <param name="sender">The sender.</param>
         /// <param name="eventArgs">The event args.</param>
+        [Obsolete("This method has been renamed, use FlagLib.Extensions.EventHandlerExtension.SafeRaise<T> instead.")]
         public static void Raise<T>(this EventHandler<T> handler, object sender, T eventArgs) where T : EventArgs
         {
             if (handler != null)
@@ -49,6 +79,7 @@ namespace FlagLib.Extensions
         /// <param name="handler">The event handler.</param>
         /// <param name="sender">The sender.</param>
         /// <param name="eventArgs">The event args.</param>
+        [Obsolete("This method has been renamed, use FlagLib.Extensions.EventHandlerExtension.SafeRaise instead.")]
         public static void Raise(this EventHandler handler, object sender, EventArgs eventArgs)
         {
             if (handler != null)
