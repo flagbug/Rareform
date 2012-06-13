1e76a1d133425c751ca7c59da57be619da53d249
Dennis Daume <daume.dennis@gmail.com>
2012-06-14 00:47:14 +0200

    Implemented some handy exception wrappers

---
+++ Rareform/Rareform/Validation/Throw.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq.Expressions;
+using Rareform.Reflection;
+
+namespace Rareform.Validation
+{
+    public static class Throw
+    {
+        public static void ArgumentException<T>(string message, Expression<Func<T>> parameterName)
+        {
+            throw new ArgumentException(message, Reflector.GetMemberName(parameterName));
+        }
+
+        public static void ArgumentException<T>(string message, Expression<Func<T>> parameterName, Exception innerException)
+        {
+            throw new ArgumentException(message, Reflector.GetMemberName(parameterName), innerException);
+        }
+
+        public static void ArgumentNullException<T>(Expression<Func<T>> parameterName)
+        {
+            throw new ArgumentNullException(Reflector.GetMemberName(parameterName));
+        }
+
+        public static void ArgumentNullException<T>(Expression<Func<T>> parameterName, string message)
+        {
+            throw new ArgumentNullException(Reflector.GetMemberName(parameterName), message);
+        }
+
+        public static void ArgumentOutOfRangeException<T>(Expression<Func<T>> parameterName)
+        {
+            throw new ArgumentOutOfRangeException(Reflector.GetMemberName(parameterName));
+        }
+
+        public static void ArgumentOutOfRangeException<T>(Expression<Func<T>> parameterName, T actualValue, string message)
+        {
+            throw new ArgumentOutOfRangeException(Reflector.GetMemberName(parameterName), actualValue, message);
+        }
+
+        public static void ArgumentOutOfRangeException<T>(Expression<Func<T>> parameterName, string message)
+        {
+            throw new ArgumentOutOfRangeException(Reflector.GetMemberName(parameterName), message);
+        }
+    }
+}
