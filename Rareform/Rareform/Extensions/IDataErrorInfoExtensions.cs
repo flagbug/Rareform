0c4400d5176b2d662f0ab9c267b6fe40c960c721
Dennis Daume <daume.dennis@gmail.com>
2013-01-17 22:37:04 +0100

    Added IDataErrorInfoExtensions

---
+++ Rareform/Rareform/Extensions/IDataErrorInfoExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Linq;
+
+namespace Rareform.Extensions
+{
+    public static class IDataErrorInfoExtensions
+    {
+        public static bool HasErrors(this IDataErrorInfo info)
+        {
+            bool hasErrors = info.GetType().GetProperties().Any(p => info[p.Name] != null);
+        }
+    }
+}
