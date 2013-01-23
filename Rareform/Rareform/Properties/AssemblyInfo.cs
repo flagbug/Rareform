639398dc151b0f7688c1bab86fb0e1ef3853b7d2
Dennis Daume <daume.dennis@gmail.com>
2013-01-24 00:10:10 +0100

    Release of version 1.4.0

--- Rareform/Rareform/Properties/AssemblyInfo.cs
+++ Rareform/Rareform/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Rareform")]
-[assembly: AssemblyCopyright("Copyright @ Dennis Daume")]
+[assembly: AssemblyCopyright("Copyright 2013 @ Dennis Daume")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
+[assembly: AssemblyVersion("1.4.0")]
+[assembly: AssemblyFileVersion("1.4.0")]
