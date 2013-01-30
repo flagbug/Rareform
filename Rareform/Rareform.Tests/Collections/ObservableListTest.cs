aa8098b610140f0876f448a8cca8e74957fadaa6
Dennis Daume <daume.dennis@gmail.com>
2013-01-30 11:27:21 +0100

    Implemented ObservableList

---
+++ Rareform/Rareform.Tests/Collections/ObservableListTest.cs
@@ -0,0 +1,146 @@
+﻿using NUnit.Framework;
+using Rareform.Collections;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Rareform.Tests.Collections
+{
+    [TestFixture]
+    public class ObservableListTest
+    {
+        [Test]
+        public void Add_FiresCollectionChanged()
+        {
+            var list = new ObservableList<int> { 1, 2 };
+
+            list.CollectionChanged += (sender, args) =>
+            {
+                Assert.AreEqual(NotifyCollectionChangedAction.Add, args.Action);
+                Assert.AreEqual(2, args.NewStartingIndex);
+                Assert.IsTrue(new[] { 3 }.SequenceEqual(args.NewItems.Cast<int>()));
+            };
+
+            list.Add(3);
+        }
+
+        [Test]
+        public void AddRange_FiresCollectionChanged()
+        {
+            var list = new ObservableList<int> { 1, 2 };
+
+            list.CollectionChanged += (sender, args) =>
+            {
+                Assert.AreEqual(NotifyCollectionChangedAction.Add, args.Action);
+                Assert.AreEqual(2, args.NewStartingIndex);
+                Assert.IsTrue(new[] { 3, 4 }.SequenceEqual(args.NewItems.Cast<int>()));
+            };
+
+            list.AddRange(new[] { 3, 4 });
+        }
+
+        [Test]
+        public void InsertRange_FiresCollectionChanged()
+        {
+            var list = new ObservableList<int> { 1, 2 };
+
+            list.CollectionChanged += (sender, args) =>
+            {
+                Assert.AreEqual(NotifyCollectionChangedAction.Add, args.Action);
+                Assert.AreEqual(1, args.NewStartingIndex);
+                Assert.IsTrue(new[] { 3, 4 }.SequenceEqual(args.NewItems.Cast<int>()));
+            };
+
+            list.InsertRange(1, new[] { 3, 4 });
+        }
+
+        [Test]
+        public void Remove_FiresCollectionChanged()
+        {
+            var list = new ObservableList<int> { 1, 2 };
+
+            list.CollectionChanged += (sender, args) =>
+            {
+                Assert.AreEqual(NotifyCollectionChangedAction.Remove, args.Action);
+                Assert.IsTrue(new[] { 2 }.SequenceEqual(args.OldItems.Cast<int>()));
+            };
+
+            list.Remove(2);
+        }
+
+        [Test]
+        public void RemoveAll_RemoveItemsAboveThreshold_FiresCollectionChanges()
+        {
+            var list = new ObservableList<int> { 1, 2, 3, 4, 4, 3, 2, 1 };
+
+            list.CollectionChanged += (sender, args) => Assert.AreEqual(NotifyCollectionChangedAction.Reset, args.Action);
+
+            list.RemoveAll(item => item == 1 || item == 3);
+        }
+
+        [Test]
+        public void RemoveAll_RemoveMultipleItems_RemovesCorrectItems()
+        {
+            var list = new ObservableList<int> { 1, 2, 3, 4, 4, 3, 2, 1 };
+
+            int removedCount = list.RemoveAll(item => item == 1 || item == 3);
+
+            Assert.AreEqual(4, removedCount);
+            Assert.IsTrue(new[] { 2, 4, 4, 2 }.SequenceEqual(list));
+        }
+
+        [Test]
+        public void RemoveAll_RemoveOneItem_RemovesCorrectItem()
+        {
+            var list = new ObservableList<int> { 1, 2, 3 };
+
+            int removedCount = list.RemoveAll(item => item == 2);
+
+            Assert.AreEqual(1, removedCount);
+            Assert.IsTrue(new[] { 1, 3 }.SequenceEqual(list));
+        }
+
+        [Test]
+        public void RemoveAll_RemovesItems_FiresCollectionChanges()
+        {
+            var list = new ObservableList<int> { 1, 2, 3, 4, 4, 3, 2, 1 };
+            int eventFireCount = 0;
+
+            list.CollectionChanged += (sender, args) =>
+            {
+                eventFireCount++;
+
+                Assert.AreEqual(NotifyCollectionChangedAction.Remove, args.Action);
+
+                switch (eventFireCount)
+                {
+                    case 1:
+                        Assert.AreEqual(0, args.OldStartingIndex);
+                        Assert.IsTrue(new[] { 1 }.SequenceEqual(args.OldItems.Cast<int>()));
+                        break;
+
+                    case 2:
+                        Assert.AreEqual(6, args.OldStartingIndex);
+                        Assert.IsTrue(new[] { 1 }.SequenceEqual(args.OldItems.Cast<int>()));
+                        break;
+                }
+            };
+
+            list.RemoveAll(item => item == 1);
+        }
+
+        [Test]
+        public void RemoveAt_FiresCollectionChanged()
+        {
+            var list = new ObservableList<int> { 1, 2 };
+
+            list.CollectionChanged += (sender, args) =>
+            {
+                Assert.AreEqual(NotifyCollectionChangedAction.Remove, args.Action);
+                Assert.AreEqual(1, args.OldStartingIndex);
+                Assert.IsTrue(new[] { 2 }.SequenceEqual(args.OldItems.Cast<int>()));
+            };
+
+            list.RemoveAt(1);
+        }
+    }
+}
---
+++ Rareform/Rareform/Collections/ObservableList.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Rareform.Collections
+{
+    public class ObservableList<T> : IList<T>, INotifyCollectionChanged
+    {
+        private readonly List<T> list;
+        private double resetThreshold;
+
+        public ObservableList()
+        {
+            this.list = new List<T>();
+            this.resetThreshold = 0.3;
+        }
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        public int Capacity
+        {
+            get { return this.list.Capacity; }
+        }
+
+        public int Count
+        {
+            get { return this.list.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public T this[int index]
+        {
+            get { return this.list[index]; }
+            set
+            {
+                T objectToReplace = this.list[index];
+
+                this.list[index] = value;
+
+                this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, value, objectToReplace, index));
+            }
+        }
+
+        public void Add(T item)
+        {
+            this.list.Add(item);
+
+            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item, this.list.Count - 1));
+        }
+
+        public void AddRange(IEnumerable<T> collection)
+        {
+            int currentCount = this.Count;
+            IList<T> itemsToAdd = collection.ToList();
+
+            this.list.AddRange(itemsToAdd);
+
+            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, (IList)itemsToAdd, currentCount));
+        }
+
+        public void Clear()
+        {
+            this.list.Clear();
+
+            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+        public bool Contains(T item)
+        {
+            return this.list.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            this.list.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public int IndexOf(T item)
+        {
+            return this.list.IndexOf(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            this.list.Insert(index, item);
+
+            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item, index));
+        }
+
+        public void InsertRange(int index, IEnumerable<T> collection)
+        {
+            IList<T> collectionToInsert = collection.ToList();
+
+            this.list.InsertRange(index, collectionToInsert);
+
+            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, (IList)collectionToInsert, index));
+        }
+
+        public bool Remove(T item)
+        {
+            bool removed = this.list.Remove(item);
+
+            if (removed)
+            {
+                this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, item));
+            }
+
+            return removed;
+        }
+
+        public int RemoveAll(Func<T, bool> match)
+        {
+            var removedList = new List<KeyValuePair<int, T>>(this.list.Capacity);
+            int previousCount = this.Count;
+
+            for (int i = 0; i < this.list.Count; i++)
+            {
+                T item = this.list[i];
+
+                if (match(item))
+                {
+                    this.list.RemoveAt(i);
+                    removedList.Add(new KeyValuePair<int, T>(i, item));
+                    i--;
+                }
+            }
+
+            if ((double)removedList.Count / previousCount > resetThreshold)
+            {
+                this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+            }
+
+            else
+            {
+                foreach (KeyValuePair<int, T> pair in removedList)
+                {
+                    this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, pair.Value, pair.Key));
+                }
+            }
+
+            return removedList.Count;
+        }
+
+        public void RemoveAt(int index)
+        {
+            T objectToRemove = this.list[index];
+
+            this.list.RemoveAt(index);
+
+            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, objectToRemove, index));
+        }
+
+        private void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            if (this.CollectionChanged != null)
+            {
+                this.CollectionChanged(this, e);
+            }
+        }
+    }
+}
